6f897e7540633e7ba36d3eddb8c48249cd7af55d
chencunming <250668663@qq.com>
2016-07-29 15:40:10 +0800

    修改ajax

--- javascript/51-map-list.js
+++ javascript/51-map-list.js
@@ -1,4 +1,6 @@
 //左侧
+var a="a0",b="b0",c="c0",d="d0",e="e0";
+var dist=[];
 $(function(){
     $(".list-classify-dist").click(function(){
         $(this).next().toggle();
@@ -8,6 +10,23 @@ $(function(){
         $(this).addClass("active");
         $(this).parents(".list-classify-item").prev().html($(this).html());
         $(this).parents(".list-classify-item").hide();
+    })
+})
+//
+$(function(){
+    $(".list-classify-item li a").click(function(){
+        var dataS=$(this)[0].dataset.s;
+        var ds=dataS.substr(0,1);
+        var dd=dataS.substr(1);
+        switch (ds) {
+            case "b":b="b"+dd;break;
+            case "c":c="c"+dd;break;
+            case "d":d="d"+dd;break;
+            case "e":e="e"+dd;break;
+        }
+        var sqlUrl=a+b+c+d+e;
+        console.log(sqlUrl);
+
     })
 })
 //地图
@@ -70,28 +89,39 @@ $(document).ready(function() {
         this._div.style.top=position.y-this._length/2+"px";
     };   
     var mapArr = [
-        [{area:'北京'},{lng:116.404,lat:39.915},{num:15}],
-        [{area:'成都'},{lng:104.144,lat:30.588},{num:3}],
-        [{area:'昆明'},{lng:102.845,lat:24.802},{num:2}],
-        [{area:'郑州'},{lng:113.623,lat:34.718},{num:2}],
-        [{area:'阜阳'},{lng:115.801,lat:32.886},{num:2}],
-        [{area:'武汉'},{lng:114.373,lat:30.451},{num:1}],
-        [{area:'长沙'},{lng:112.919,lat:28.276},{num:2}],
-        [{area:'广州'},{lng:113.272,lat:23.147},{num:2}],
-        [{area:'深圳'},{lng:114.043,lat:22.542},{num:1}],
-        [{area:'汕头'},{lng:116.669,lat:23.371},{num:2}],
-        [{area:'扬州'},{lng:119.456,lat:32.374},{num:1}],
-        [{area:'苏州'},{lng:120.587,lat:31.298},{num:1}],
-        [{area:'上海'},{lng:121.492,lat:31.912},{num:4}],
+        [{area:'北京'},{lng:116.404,lat:39.915},{num:15},{a:"a0"}],
+        [{area:'成都'},{lng:104.144,lat:30.588},{num:3},{a:"a1"}],
+        [{area:'昆明'},{lng:102.845,lat:24.802},{num:2},{a:"a2"}],
+        [{area:'郑州'},{lng:113.623,lat:34.718},{num:2},{a:"a3"}],
+        [{area:'阜阳'},{lng:115.801,lat:32.886},{num:2},{a:"a4"}],
+        [{area:'武汉'},{lng:114.373,lat:30.451},{num:1},{a:"a5"}],
+        [{area:'长沙'},{lng:112.919,lat:28.276},{num:2},{a:"a6"}],
+        [{area:'广州'},{lng:113.272,lat:23.147},{num:2},{a:"a7"}],
+        [{area:'深圳'},{lng:114.043,lat:22.542},{num:1},{a:"a8"}],
+        [{area:'汕头'},{lng:116.669,lat:23.371},{num:2},{a:"a9"}],
+        [{area:'扬州'},{lng:119.456,lat:32.374},{num:1},{a:"a10"}],
+        [{area:'苏州'},{lng:120.587,lat:31.298},{num:1},{a:"a11"}],
+        [{area:'上海'},{lng:121.492,lat:31.912},{num:4},{a:"a12"}],
         
     ];
     function addOverlay(mapArr){
+            var distarr=[];
             for (var i = 0; i < mapArr.length; i++) {
-            var mySquare=new square(mapArr[i][1],80,mapArr[i][0].area,mapArr[i][2].num);
-            map.addOverlay(mySquare);
+            var mySquare=new square(mapArr[i][1],80,mapArr[i][0].area,mapArr[i][2].num);            
+            if(testInnerMap(mapArr[i][1]))
+                {   
+                    map.addOverlay(mySquare);
+                    distarr.push(mapArr[i][3].a);
+                }
             }
+            dist=distarr;
+            console.log(dist);
         }
-    addOverlay(mapArr);
+    addOverlay(mapArr);      
+    map.addEventListener("dragging", function(){
+        addOverlay(mapArr); 
+    })
+    // addOverlay(mapArr);
         if (map.getZoom()<6) {
             map.clearOverlays()
         };
@@ -100,7 +130,17 @@ $(document).ready(function() {
                 map.clearOverlays()
             }
             else{
-                addOverlay(mapArr)
+                addOverlay(mapArr);
             }
         });
+    function testInnerMap(obj){
+        var bs = map.getBounds();   //获取可视区域
+        var bssw = bs.getSouthWest();   //可视区域左下角
+        var bsne = bs.getNorthEast();   //可视区域右上角   
+        if(obj.lng<bsne.lng && obj.lng>bssw.lng && obj.lat<bsne.lat && obj.lat>bssw.lat){
+            return true;
+        }else{
+            return false;
+        }   
+    }
 })
--- javascript/51-watch-list.js
+++ javascript/51-watch-list.js
@@ -1,11 +1,45 @@
+$(function() {
+    if ($(document).width() < 767) {
+            $(".list-classify-dist").click(function() {
+                $(this).next().toggle();
+            });
+            $(".list-classify-item li a").click(function() {
+                $(".list-classify-item li a").removeClass("active");
+                $(this).addClass("active");
+                $(this).parents(".list-classify-item").prev().html($(this).html());
+                $(this).parents(".list-classify-item").hide();
+            })
+        }   
+    $(window).resize(function() {
+        if ($(document).width() < 766) {
+            $(".list-classify-dist").click(function() {
+                $(this).next().toggle();
+            });
+            $(".list-classify-item li a").click(function() {
+                $(".list-classify-item li a").removeClass("active");
+                $(this).addClass("active");
+                $(this).parents(".list-classify-item").prev().html($(this).html());
+                $(this).parents(".list-classify-item").hide();
+            })
+        }
+    })    
+})
 $(function(){
-    $(".list-classify-dist").click(function(){
-        $(this).next().toggle();
-    });
+    var a="a0",b="b0",c="c0",d="d0",e="e0";
     $(".list-classify-item li a").click(function(){
-        $(".list-classify-item li a").removeClass("active");
-        $(this).addClass("active");
-        $(this).parents(".list-classify-item").prev().html($(this).html());
-        $(this).parents(".list-classify-item").hide();
+        var dataS=$(this)[0].dataset.s;
+        var ds=dataS.substr(0,1);
+        var dd=dataS.substr(1);
+        switch (ds) {
+            case "a":a="a"+dd;break;
+            case "b":b="b"+dd;break;
+            case "c":c="c"+dd;break;
+            case "d":d="d"+dd;break;
+            case "e":e="e"+dd;break;
+        }
+        var sqlUrl=a+b+c+d+e;
+        console.log(sqlUrl);
+
     })
 })
+
